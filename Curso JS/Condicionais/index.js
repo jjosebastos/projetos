2323a3a63c1065201f2664b3f8a59d04b4be1c1b
Jose Bezerra <josebezerrabastos@gmail.com>
2023-03-04 00:27:37 -0300

    Preparando o diretório

---
+++ Curso JS/Condicionais/index.js
@@ -0,0 +1,25 @@
+/*
+
+let num = 1;
+
+if (num >= 12) {
+    console.log('Boa tarde!')
+} else if (num >= 18 ) {
+    console.log('Bom noite!');
+} else {
+    console.log('Bom dia!');
+}
+
+*/
+
+let num = prompt('Digite um numero'); 
+
+if (num >= 5 && num <= 11 ){
+    console.log('Bom dia!');
+}else if (num >= 12 && num <= 18 ){
+    console.log('Boa tarde!')
+}else if (num >= 19 && num <= 23 ){
+
+}else {
+    console.log('Numero inválido!');
+}
